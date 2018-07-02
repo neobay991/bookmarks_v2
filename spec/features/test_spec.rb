bcc319f2a5e5661b6060f6393bee0702840775aa
Jay Khan <jkhan992@gmail.com>
2018-07-02 15:06:47 +0100

    refactor: updated test

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -1,7 +1,8 @@
 require "./bookmark_app.rb"
 
 feature "Testing" do
-  scenario "Can run a app"
+  scenario "Can run a app" do
     visit("/server")
     expect(page).to have_content("Testing")
+  end
 end
