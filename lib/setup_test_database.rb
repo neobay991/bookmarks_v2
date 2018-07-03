18c565cf95b69dfdd1df22a82ba95a23e03876d2
Jay Khan <jkhan992@gmail.com>
2018-07-03 16:54:51 +0100

    Refactor: added comment

--- lib/setup_test_database.rb
+++ lib/setup_test_database.rb
@@ -1,12 +1,12 @@
 require 'pg'
 
   p "setting up test database"
-
+  
   connection = PG.connect(dbname: 'Bookmark_manager_test')
 
   # clear the bookmarks table
   connection.exec("TRUNCATE bookmarks;")
 
-  
+  p "inserting bookmark in test database"
   # insert a test bookmark in the bookmarks table
   connection.exec("INSERT INTO bookmarks VALUES(1, 'http://test.com');")
