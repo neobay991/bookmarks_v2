5af926aaa1ee46baa83127f264e8d2bf5795e4fc
Jay Khan <jkhan992@gmail.com>
2018-07-03 14:40:26 +0100

    added pg gemfile and connection to database

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,11 +1,14 @@
+require 'pg'
+
 class Bookmarks
-
   def self.all
-    [
-      "http://makersacademy.com",
-      "http://destroyallsoftware.com",
-      "http://google.com"
-    ]
+      connection = PG.connect(dbname: 'Bookmark_manager')
+      result = connection.exec("SELECT * FROM bookmarks")
+      result.map { |bookmark| bookmark['url'] }
+    # [
+    #   "http://makersacademy.com",
+    #   "http://destroyallsoftware.com",
+    #   "http://google.com"
+    # ]
   end
-
 end
